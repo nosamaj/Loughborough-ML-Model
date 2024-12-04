f0a535139e915b652af9cde3e4e450b9a0683d13
James Mason <nosamaj@gmail.com>
2024-12-04 20:44:12 +0000

    initial commit

---
+++ train_xgboost.py
@@ -0,0 +1,11 @@
+import pandas as pd 
+import xgboost as xgb
+
+def define_feature_and_label(dataframe,feature_col_names,label_col_names) -> pd.dataframe:
+
+
+
+x_data 
+
+
+model = xgb.Regressor()
